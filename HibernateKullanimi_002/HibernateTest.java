404757dac9efa52d7fbcce694a0adcd472dc639d
hanifekurnaz <hanife.kurnaz@bil.omu.edu.tr>
2017-06-16 10:38:34 +0300

    Create HibernateTest.java

---
+++ HibernateKullanimi_002/HibernateTest.java
@@ -0,0 +1,23 @@
+package com.hibernate.test;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernatetutorial.Personel;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Personel personel = new Personel();
+		personel.setPersonelid(1);
+		personel.setPersonelAdi("Hanife");
+		personel.setPersonelid(2);
+		personel.setPersonelAdi("Ayse");
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(personel);
+		session.getTransaction().commit();
+	}
+}
