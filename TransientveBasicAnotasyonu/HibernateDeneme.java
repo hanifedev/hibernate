0b80a5293b76ec4a421840fc8aef3e8122306164
hanifekurnaz <hanifekurnaz@outlook.com>
2017-06-20 12:23:58 +0300

    @Transient ve @Basic anotasyonlari kullanimi

---
+++ TransientveBasicAnotasyonu/HibernateDeneme.java
@@ -0,0 +1,27 @@
+package com.hibernatetest;
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.entity.Personel;
+
+public class HibernateDeneme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Personel personel = new Personel();
+		personel.setPersonelid(2);
+		personel.setPersonelAdi("Hanife");
+		personel.setPersonelSoyadi("Kurnaz");
+		personel.setKayitTarihi(new Date());
+		personel.setPersonelAdresi("Samsun Turkiye");
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory(); //tanimlanan oturumlari ureten fabrika sinifi
+		Session session = sessionFactory.openSession(); //oturumu ac
+		session.beginTransaction(); //transaction baslangic
+		session.save(personel); //veritabanina kaydet
+		session.getTransaction().commit();	//transaction bitis
+		session.close(); // oturum nesnesi sonlandirma
+		}
+}
