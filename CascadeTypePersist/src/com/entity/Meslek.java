abd68bb9e32d611bcc170139fefdd499e1599ddc
hanifekurnaz <hanifekurnaz@outlook.com>
2017-06-22 14:22:48 +0300

    cascade type kullanimi

---
+++ CascadeTypePersist/src/com/entity/Meslek.java
@@ -0,0 +1,43 @@
+package com.entity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="meslek")
+public class Meslek {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int meslekId;
+	private String meslekAdi;
+	
+	@ManyToMany(mappedBy="meslekListesi")
+	private Collection<Personel> personelListesi = new ArrayList<Personel>();
+	
+	public Collection<Personel> getPersonelListesi() {
+		return personelListesi;
+	}
+	public void setPersonelListesi(Collection<Personel> personelListesi) {
+		this.personelListesi = personelListesi;
+	}
+	public int getMeslekId() {
+		return meslekId;
+	}
+	public void setMeslekId(int meslekId) {
+		this.meslekId = meslekId;
+	}
+	public String getMeslekAdi() {
+		return meslekAdi;
+	}
+	public void setMeslekAdi(String meslekAdi) {
+		this.meslekAdi = meslekAdi;
+	}
+	
+}
---
+++ CascadeTypePersist/src/com/hibernatetest/HibernateDeneme.java
@@ -0,0 +1,35 @@
+package com.hibernatetest;
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.entity.Meslek;
+import com.entity.Personel;
+
+public class HibernateDeneme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Personel personel = new Personel();
+		personel.setPersonelAdi("Hanife");
+		personel.setPersonelSoyadi("Kurnaz");
+		personel.setKayitTarihi(new Date());
+		
+		Meslek meslek = new Meslek();
+		meslek.setMeslekAdi("bilgisayar muhendisi");
+		
+		Meslek meslek2 = new Meslek();
+		meslek2.setMeslekAdi("bilgisayar ogretmeni");
+		
+		personel.getMeslekListesi().add(meslek);
+		personel.getMeslekListesi().add(meslek2);
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory(); //tanimlanan oturumlari ureten fabrika sinifi
+		Session session = sessionFactory.openSession(); //oturumu ac
+		session.beginTransaction(); //transaction baslangic
+		session.persist(personel); //nesne ile iliskili olan tum verileri kaydeder
+		session.getTransaction().commit();	//transaction bitis
+		session.close(); // oturum nesnesi sonlandirma
+	}
+}
